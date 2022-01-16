020392f969ea361892fb09c215a5a91d2b5652bd
jwfu <pypi.solecistic@aleeas.com>
2022-01-16 19:02:50 +0000

    ch2

---
+++ ch3/HelloWorld.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.0;
+
+contract HelloWorld {
+    string public message;
+
+    function setMessage(string newMesnsage) public {
+        message = newMessage;
+    }
+
+    function remove() public {
+        selfdestruct(0x0);
+    }
+}
